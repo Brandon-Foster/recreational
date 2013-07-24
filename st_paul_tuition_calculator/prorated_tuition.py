6e8348f26044f7ed3b994ef9081bd910cecb8bef
Brandon-Foster <brandon.foster.vt@gmail.com>
2013-07-23 22:03:53 -0400

    Accept year, month, and date from user

--- st_paul_tuition_calculator/prorated_tuition.py
+++ st_paul_tuition_calculator/prorated_tuition.py
@@ -1,11 +1,42 @@
 # Author: Brandon Foster
 # Date: July 22, 2013
 #
-# Calculate prorated tuition for a child at St. Paul School
+# Calculate prorated tuition of a given month for a child at St. Paul School
 
-print ("Please enter the month of the student's start date using proper case "
-	"(i.e. 'January').")
+from datetime import date
 
-month = raw_input("\n> ")
+months = {
+	1: 'January',
+	2: 'February',
+	3: 'March',
+	4: 'April',
+	5: 'May',
+	6: 'June',
+	7: 'July',
+	8: 'August',
+	9: 'September',
+	10: 'October',
+	11: 'November',
+	12: 'December'
+}
 
-print "The month you entered is: %s." % month
+# Accepts the starting year
+print ("\nPlease enter the year the student began. (i.e., 2013)")
+
+year = int(raw_input("\n> "))
+
+print "\nYou entered %d." % (year)
+
+# Accept the starting month
+print ("\nPlease enter the month the student began. (January would be 1)")
+
+month = int(raw_input("\n> "))
+
+print "\nYou entered %d: %s." % (month, months[month])
+
+# Accepts the starting date
+print ("\nPlease enter the start date of the student.")
+
+date = int(raw_input("\n> "))
+
+print "\nThe date you entered is: %d." % (date)
