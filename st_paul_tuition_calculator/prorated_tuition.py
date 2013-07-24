47b991fdebc0d981993ec708c845b368abeaace6
Brandon-Foster <brandon.foster.vt@gmail.com>
2013-07-23 20:48:49 -0400

    Add st_paul_tuition_calculator

---
+++ st_paul_tuition_calculator/prorated_tuition.py
@@ -0,0 +1,11 @@
+# Author: Brandon Foster
+# Date: July 22, 2013
+#
+# Calculate prorated tuition for a child at St. Paul School
+
+print ("Please enter the month of the student's start date using proper case "
+	"(i.e. 'January').")
+
+month = raw_input("\n> ")
+
+print "The month you entered is: %s." % month
